592189406850e8a34a8932adc3f92e233c3cfede
sulm <sulm@gkomega.ru>
2020-06-10 14:33:50 +0300

    Changed the structure of the header

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Logo from "../logo";
+
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <header className="navbar navbar-light bg-light  header">
+      <Logo />
+    </header>
+  );
+};
+
+export default Header;
