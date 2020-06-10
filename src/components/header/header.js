9e1c098cd07a6e943bb5d357f6b3a75db7e425f4
sulm <sulm@gkomega.ru>
2020-06-10 16:03:51 +0300

    Added authorization component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,12 +4,14 @@ import Logo from "../logo";
 import Address from "../address/address";
 
 import "./header.scss";
+import Authorization from "../authorization";
 
 const Header = () => {
   return (
     <header className="navbar navbar-light bg-light  header">
       <Logo />
       <Address />
+      <Authorization />
     </header>
   );
 };
